a62c6fcd63b97b24c4e6186a8204a239a36fba19
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2023-03-04 00:08:25 +0700

    userPage V1

--- Flask_Run_Application/app.py
+++ Flask_Run_Application/app.py
@@ -1,3 +1,13 @@
+from werkzeug.utils import secure_filename
+import json
+import pandas as pd
+from scipy.spatial import distance
+from PIL import Image
+from tensorflow import keras
+import tensorflow_hub as hub
+import tensorflow as tf
+import numpy as np
+import os
 from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
@@ -6,18 +16,8 @@ app = Flask(__name__)
 app.secret_key = 'itm_group_4'
 # connection Database
 
-#model library
-import os
+# model library
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-import numpy as np
-import tensorflow as tf
-import tensorflow_hub as hub
-from tensorflow import keras
-from PIL import Image
-from scipy.spatial import distance
-import pandas as pd
-import json
-from werkzeug.utils import secure_filename
 
 
 def openConnection():
@@ -29,11 +29,16 @@ def openConnection():
                                  #  cursorclass=pymysql.cursors.DictCursor
                                  )
     return connection
+
+
 # localhost, username, password, database
 # =======================================================
 # Model
-model = keras.models.load_model('D:/ITM_Group4_Reverse_Image_Search_for_Online_Shopping/Model/preweight/efficientnet.h5',custom_objects={'KerasLayer':hub.KerasLayer})
+model = keras.models.load_model(
+    'D:/ITM_Group4_Reverse_Image_Search_for_Online_Shopping/Model/preweight/efficientnet.h5', custom_objects={'KerasLayer': hub.KerasLayer})
 model.summary()
+
+
 def extract(file):
     IMAGE_SHAPE = (224, 224)
     file = Image.open(file).convert('L').resize(IMAGE_SHAPE)
@@ -44,14 +49,15 @@ def extract(file):
     flattended_feature = vgg16_feature_np.flatten()
     return flattended_feature
 
+
 def searchVector(vector):
     con = openConnection()
-    cur = con.cursor() 
+    cur = con.cursor()
     cur.execute("Select * From product")
-    rows=cur.fetchall()
+    rows = cur.fetchall()
     data = {
-        "id": [], #row[0]
-        "distance": [] #row[4] = vector
+        "id": [],  # row[0]
+        "distance": []  # row[4] = vector
     }
     df = pd.DataFrame(data)
     metric = 'cosine'
@@ -59,7 +65,7 @@ def searchVector(vector):
         r = np.array(json.loads(row[4]))
         print(type(r))
         dc = distance.cdist([vector], [r], metric)[0]
-        df.loc[len(df.index)] = [row[0],dc]
+        df.loc[len(df.index)] = [row[0], dc]
     return df.sort_values(by=['distance'])
 
 # =======================================================
@@ -80,7 +86,7 @@ def shoppingWithoutUser():
     cur.execute(sql)
     result_product = cur.fetchall()
     conn.close()
-    return render_template('pageWithoutUser/shop.html', product = result_product)
+    return render_template('pageWithoutUser/shop.html', product=result_product)
 
 
 @app.route("/search")
@@ -97,6 +103,7 @@ def signInPage():
 def signUpPage():
     return render_template('pageWithoutUser/signup.html')
 
+
 @app.route("/aboutUs")
 def aboutUsPage():
     return render_template('pageWithoutUser/aboutUs.html')
@@ -105,10 +112,12 @@ def aboutUsPage():
 
 # action
 
+
 @app.route('/logout')
 def logout():
     session.pop('user', None)
     return redirect(url_for('signInPage'))
+
 
 @app.route("/sign_in/actions", methods=['POST'])
 def signInLogin():
@@ -127,12 +136,12 @@ def signInLogin():
         userType = result[6]
         conn.close()
         if result and check_password_hash(result[5], password):
-        # if result :
+            # if result :
             session['user'] = result[1]
             if userType == "ADMIN":
                 return redirect(url_for('adminPageUser', userType_name=result[6], user_id=result[1]))
             elif (userType == "USER"):
-                return "USER LOGIN"
+                return redirect(url_for('userPageShopping', userType_name=result[6], user_id=result[1]))
         else:
             return redirect(url_for('signInPage'))
     else:
@@ -155,6 +164,40 @@ def signUpLogin():
     conn.close()
     # INSERT INTO `user`(`user_id`, `user_email`, `user_fname`, `user_lname`, `user_password`, `userType_id`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')
     return redirect(url_for('signInPage'))
+
+# action adding product
+
+
+@app.route("/<string:userType_name>/<string:user_id>/insert_product", methods=['POST'])
+def adminPageActionAddingProduct(userType_name, user_id):
+    if request.method == "POST":
+        product_name = request.form['product_name']
+        product_color = request.form['product_color']
+        product_cost = request.form['product_cost']
+        product_category = request.form['product_category']
+        file_image = request.files['product_image']
+        filename = secure_filename(product_name+".jpg")
+
+        # path folder for save img for upload
+        file_image.save(
+            'D:/ITM_Group4_Reverse_Image_Search_for_Online_Shopping/Flask_Run_Application/static/img_product_into_db/' + filename)
+
+        # path folder for save img for call img into database with feature
+        folder_path = "D:/ITM_Group4_Reverse_Image_Search_for_Online_Shopping/Flask_Run_Application/static/img_product_into_db/{}".format(
+            filename)
+        feature = extract(folder_path)
+        js = json.dumps(feature.tolist())
+        product_vector = js
+
+        con = openConnection()
+        cur = con.cursor()
+        sql = "INSERT INTO `product` (`product_name`, `product_img_name`, `product_cost`, `product_vector`, `product_color`, `productCategory_id`) VALUES (%s, %s, %s, %s, %s, %s)"
+        cur.execute(sql, (product_name, filename, product_cost,
+                    product_vector, product_color, product_category))
+        con.commit()
+        con.close()
+
+        return redirect(url_for('adminPageProduct', userType_name=userType_name, user_id=user_id))
 
 # =======================================================
 # With User Login
@@ -193,44 +236,40 @@ def adminPageProduct(userType_name, user_id):
 def adminPageAddProduct(userType_name, user_id):
     if 'user' in session:
         con = openConnection()
-        cur = con.cursor() 
+        cur = con.cursor()
         cur.execute("SELECT * FROM `product_category`")
         product_category_result = cur.fetchall()
-        return render_template('pageWithUser/Adminpage/Addproduct/addproduct.html', data_id=user_id, data_userType=userType_name, product_category = product_category_result)
+        return render_template('pageWithUser/Adminpage/Addproduct/addproduct.html', data_id=user_id, data_userType=userType_name, product_category=product_category_result)
     else:
         return redirect(url_for('signInPage'))
 
 
-# action adding product
-@app.route("/<string:userType_name>/<string:user_id>/insert_product", methods=['POST'])
-def adminPageActionAddingProduct(userType_name, user_id):
-    if request.method=="POST":
-        product_name = request.form['product_name']
-        product_color = request.form['product_color']
-        product_cost = request.form['product_cost']
-        product_category = request.form['product_category']
-        file_image = request.files['product_image']
-        filename = secure_filename(product_name+".jpg")
-
-        # path folder for save img for upload
-        file_image.save('D:/ITM_Group4_Reverse_Image_Search_for_Online_Shopping/Flask_Run_Application/static/img_product_into_db/'+ filename)
-        
-        # path folder for save img for call img into database with feature
-        folder_path = "D:/ITM_Group4_Reverse_Image_Search_for_Online_Shopping/Flask_Run_Application/static/img_product_into_db/{}".format(filename)
-        feature = extract(folder_path)
-        js = json.dumps(feature.tolist())
-        product_vector = js
-
-        con = openConnection()
-        cur = con.cursor()
-        sql="INSERT INTO `product` (`product_name`, `product_img_name`, `product_cost`, `product_vector`, `product_color`, `productCategory_id`) VALUES (%s, %s, %s, %s, %s, %s)"
-        cur.execute(sql,(product_name, filename, product_cost, product_vector, product_color, product_category))
-        con.commit()
-        con.close()
-
-        return redirect(url_for('adminPageProduct', userType_name = userType_name, user_id = user_id))
-
-
+@app.route("/<string:userType_name>/<string:user_id>/shopping")
+def userPageShopping(userType_name, user_id):
+    if 'user' in session:
+        conn = openConnection()
+        cur = conn.cursor()
+        sql = "SELECT * FROM `product` NATURAL JOIN product_category"
+        cur.execute(sql)
+        result_product = cur.fetchall()
+        conn.close()
+        return render_template('pageWithUser/Userpage/user.html', data_id=user_id, data_userType=userType_name, product=result_product)
+    else:
+        return redirect(url_for('signInPage'))
+    
+@app.route("/<string:userType_name>/<string:user_id>/search")
+def userPageSearch(userType_name, user_id):
+    if 'user' in session:
+        return render_template('pageWithUser/Userpage/userSearch.html', data_id=user_id, data_userType=userType_name)
+    else:
+        return redirect(url_for('signInPage'))
+    
+@app.route("/<string:userType_name>/<string:user_id>/about_us")
+def userPageAboutUs(userType_name, user_id):
+    if 'user' in session:
+        return render_template('pageWithUser/Userpage/userAboutUs.html', data_id=user_id, data_userType=userType_name)
+    else:
+        return redirect(url_for('signInPage'))
 # =======================================================
 
 
